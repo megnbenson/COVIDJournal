e2d2b6e513419efb9cf41ed854ac3017a0f3aa41
jkchuong <jkchuong@gmail.com>
2021-03-10 17:31:58 +0000

    Initial Commit

---
+++ CovidJournal/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CovidJournal.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidJournal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CovidUser> CovidUser { get; set; }
+        public DbSet<CovidEntry> CovidEntry { get; set; }
+    }
+}
