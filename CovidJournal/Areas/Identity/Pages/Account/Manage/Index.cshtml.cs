daaf7a36e759f47dadc1962964d00cb84a427acb
jkchuong <jkchuong@gmail.com>
2021-03-11 16:18:10 +0000

    Changed registration page to have first and last name

--- CovidJournal/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ CovidJournal/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -36,6 +36,16 @@ namespace CovidJournal.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
         }
 
         private async Task LoadAsync(ApplicationUser user)
@@ -47,7 +57,9 @@ namespace CovidJournal.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName
             };
         }
 
@@ -87,6 +99,11 @@ namespace CovidJournal.Areas.Identity.Pages.Account.Manage
                     return RedirectToPage();
                 }
             }
+
+            if (Input.FirstName != user.FirstName) { user.FirstName = Input.FirstName; }
+            if (Input.LastName != user.LastName) { user.LastName = Input.LastName; }
+            await _userManager.UpdateAsync(user);
+
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
--- CovidJournal/Models/ApplicationUser.cs
+++ CovidJournal/Models/ApplicationUser.cs
@@ -8,6 +8,7 @@ namespace CovidJournal.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
